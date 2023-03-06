3f548c34c234f11ce181380648e6849ac7af8275
GitHub Action <action@github.com>
2023-03-06 17:52:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/dayux/shrektheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dayux/shrektheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dayux.shrektheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dayux.shrektheme.MyBundle
 
 class MyApplicationService {
 
